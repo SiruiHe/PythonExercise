30abe0b01ffa04c5ffe332f8ddc6e35c28a5012c
SiruiHe <46714611+SiruiHe@users.noreply.github.com>
2021-04-23 19:10:00 +0800

    ver3文件只能内网传，ver3.1配合花生壳工具（需要在接收方设置），ver3.5经由服务器转发、发送方发送完毕后指令顺序发生错乱。总体而言不是很满意。

---
+++ chatapp/client3.5.py
@@ -0,0 +1,252 @@
+import threading, time, socket,os,struct,requests
+#服务器公网ip地址
+host ="127.0.0.1"#"81.70.245.218"
+port = 8888
+addr0 = (host, port)
+clinet_receive_flag = 0
+
+
+def sendmsg(skt, msg="", head=""):
+    if len(head) > 0:
+        msg = head + msg
+    skt.send(msg.encode("utf-8"))
+
+
+def client_signup(skt):
+    print("欢迎注册！")
+    guide = "signup_guide$"
+    skt.send(guide.encode("utf-8"))
+    while (1):
+        username = input("请输入您想使用的用户名：")
+        text = "signup_username$" + username
+        skt.send(text.encode("utf-8"))
+        msg = skt.recv(100).decode("utf-8")
+
+        password = input("请输入您想设置的密码：")
+        text = "signup_password$" + password
+        skt.send(text.encode("utf-8"))
+        msg = skt.recv(100).decode("utf-8")
+
+        if (msg.lower() == "signup_ok"):
+            print("注册成功,转入登陆界面")
+            break
+        else:
+            print("注册失败,请重试")
+    client_login(skt)
+    return
+
+
+def client_login(skt):
+    print("欢迎登录！")
+    guide = "login_guide$"
+    skt.send(guide.encode("utf-8"))
+    while (1):
+        username = input("请输入您的用户名：")
+        sendmsg(skt, username, "login_username$")
+
+        password = input("请输入您的密码：")
+        sendmsg(skt, password, "login_password$")
+        msg = skt.recv(100).decode("utf-8")
+        if (msg.lower() == "login_ok"):
+            print("登录成功")
+            break
+        else:
+            print("登录失败,请重试")
+
+    # 转入本客户机发送过程
+    client_online(skt)
+    return
+
+
+def client_online(skt):
+    global clinet_receive_flag
+    print("登陆成功！")
+    while (1):
+        clinet_receive_flag = 0
+        # stop=0
+        while (1):
+            # 获取在线用户
+            sendmsg(skt, "get_usertemp$")
+            msg = skt.recv(100).decode("utf-8")
+
+            print("\n当前在线的用户有：\n" + msg)
+            chatwith = input("请输入用户的名字：")
+            sendmsg(skt, chatwith, "online_or_not$")
+            msg = skt.recv(100).decode("utf-8")
+            if (msg.startswith("online$")):
+                break
+            if (msg.startswith("offline$")):
+                print("无法与此用户取得联系，请重试。")
+
+
+        while(1):
+            print("请选择您想做的事情：")
+            print("1.聊天\t2.发文件\t3.收文件\t4.退出程序\t")
+            choice2=int(input(">>"))
+            if(choice2 in [1,2,3,4]):
+                break
+
+        # 开始聊天
+        if(choice2==1):
+            print("向用户 " + chatwith + " 发送消息")
+            clinet_receive = threading.Thread(target=clinet_thread_receive, args=(skt,))
+            clinet_receive.start()
+
+            while (1):
+                msg = input()
+                if(len(msg)==0):
+                    break
+                sendmsg(skt, chatwith + "$" + msg, "chat_with_sb$")
+                if (msg.startswith("byebye")):
+                    sendmsg(skt, "say_byebye$")
+                    clinet_receive_flag = 1
+                    time.sleep(0.2)
+                    clinet_receive.join()
+                    break
+
+        #发文件
+        elif(choice2==2):
+            print("向用户 " + chatwith + " 传输文件")
+            print("接收方准备中...")
+            sendmsg(skt, chatwith + "$", "send_file_request$")
+            msg=skt.recv(100).decode("utf-8")
+
+            if(msg.startswith("send_file_permission$")):
+                clinet_send_file(skt,chatwith)
+
+        elif (choice2 == 3):
+            print("从用户 " + chatwith + " 接收文件")
+            sendmsg(skt, chatwith + "$", "receive_file_request$")
+            msg = skt.recv(100).decode("utf-8")
+
+            if (msg.startswith("receive_file_permission$")):
+                clinet_receive_file(skt)
+
+        elif (choice2 == 4):
+            return
+
+
+
+
+def clinet_send_file(skt,chatwith):
+    print("欢迎来到发送界面！")
+    sendmsg(skt,chatwith+"$","start_send_file$")
+    count = 0
+    while (1):
+        filepath = input("输入要发送的文件路径:")
+        #if len(filepath) == 0:
+        #    filepath = r"C:\Users\sword\Pictures\老中青.JPG"
+        if os.path.isfile(filepath):
+            fileinfo_size = struct.calcsize("128sl")
+            filehead = struct.pack("128sl", bytes(os.path.basename(filepath).encode('utf-8')),
+                                   os.stat(filepath).st_size)
+            skt.send(filehead)
+            print("接收方文件路径:{}".format(filepath))
+
+            fp = open(filepath, "rb")
+            while (1):
+                data = fp.read(1024)
+                count += len(data)
+                if not data:
+                    print("{0} 此文件发送完成.".format(filepath))
+                    break
+                skt.send(data)
+                process = count / os.stat(filepath).st_size * 100
+                # print("Receiving process: " + "%.2f" % process + "%")
+                print("发送中: " + "%.2f" % process + "%")
+            sendmsg(skt,"send_file_over$")
+            #skt_send.close()
+            break
+    time.sleep(0.3)
+    return
+
+def clinet_receive_file(skt):
+    print("欢迎来到接收界面！")
+
+    while(1):
+        msg=skt.recv(100).decode("utf-8")
+        print(msg)
+        if(msg.startswith("file_ready$")):
+            print("连接成功！")
+            break
+
+    while (1):
+        fileinfo_size = struct.calcsize("128sl")
+        buf = skt.recv(fileinfo_size)
+        if buf:
+            filename, filesize = struct.unpack("128sl", buf)
+
+            # 去除空格
+            fn = filename.decode().strip("\00")
+
+            new_filename = os.path.join("./", "new_" + fn)
+            print("接收的新文件为 {0},文件大小为 {1}".format(new_filename, filesize))
+
+            recvd_size = 0
+            fp = open(new_filename, "wb")
+            print("开始接收文件...")
+
+            # while not recvd_size==filesize:
+            while recvd_size < filesize:
+                if (filesize - recvd_size > 1024):
+                    data = skt.recv(1024)
+                    recvd_size += len(data)
+                    process = recvd_size / filesize * 100
+                    print("接收中: " + "%.2f" % process + "%")
+                else:
+                    data = skt.recv(filesize - recvd_size)
+                    recvd_size = filesize
+                fp.write(data)
+            fp.close()
+            print("接收完毕!")
+        #conn.close()
+        break
+    sendmsg(skt,"receive_file_over$")
+    return
+
+
+
+def clinet_thread_receive(skt):
+    global clinet_receive_flag
+    while (1):
+        if (clinet_receive_flag == 1):
+            break
+        msg = skt.recv(100).decode("utf-8")
+        if (msg.startswith("byebye$")):
+            break
+        print("\n" + msg)
+    print("监听线程退出。继续...")
+    # return
+
+
+def main():
+    skt = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    skt.connect(addr0)
+    print("连接成功。请选择...")
+    print("1.注册\t2.登陆\t")
+    choice1 = input(">>")
+    if (choice1 == "1"):
+        client_signup(skt)
+    if (choice1 == "2"):
+        client_login(skt)
+
+    sendmsg(skt,str(skt.getsockname()),"logout$")
+    skt.close()
+    print("再见!")
+    '''
+    while(1):
+        data=input("输入：")
+        if not data:
+            break
+        skt.send(data.encode("utf-8"))
+        data=skt.recv(100)
+        if not data:
+            break
+        print("收到消息:"+data.decode("utf-8"))
+    skt.close()
+    '''
+
+
+
+if __name__ == '__main__':
+    main()
---
+++ chatapp/server3.1.py
@@ -0,0 +1,227 @@
+import threading,socket,time,shelve
+
+host="0.0.0.0" #??
+port=8888
+addr0=(host,port)
+global usermap
+
+
+userstate={}    #[用户名：socket]的字典
+
+def sendmsg(skt,msg,head=""):
+    if len(head)>0:
+        msg=head+msg
+    skt.send(msg.encode("utf-8"))
+
+'''
+class User:
+    def __init__(self,skt,username="null"):
+        self.skt=skt
+        self.username=username
+        #self.password=password
+    def sendmsg(self,msg):
+        self.skt.send(msg.encode("utf-8"))
+    def logout(self):
+        self.skt.close()
+'''
+
+def server_signup(skt):
+    global usermap
+    flag=0
+    while(1):
+        msg=skt.recv(100).decode("utf-8")
+        msg=msg.split("$")
+        if(msg[0]=="signup_username"):
+            username=msg[1]
+            flag+=1
+            text = "ok"
+            skt.send(text.encode("utf-8"))
+
+        if(msg[0]=="signup_password"):
+            password=msg[1]
+            flag+=1
+        if flag==2:
+            break
+    usermap[username]=password
+    text="signup_ok"
+    #打出来看一下
+    print(usermap[username])
+
+    skt.send(text.encode("utf-8"))
+    server_login(skt)
+
+
+def server_login(skt):
+    global usermap
+    flag=0
+    username,password="",""
+    while(1):
+        msg=skt.recv(100).decode("utf-8")
+        msg=msg.split("$")
+        if(msg[0]=="login_username"):
+            username=msg[1]
+        if(msg[0]=="login_password"):
+            password=msg[1]
+        if len(username)>0 and len(password)>0:
+            try:
+                if(usermap[username]==password):
+                    break
+                else:
+                    sendmsg(skt,"login_failed")
+                    username,password="",""
+            except Exception:
+                sendmsg(skt, "login_failed")
+                username, password = "", ""
+    sendmsg(skt,"login_ok")
+    server_user_online(skt,username)
+
+
+
+def server_user_online(skt,username):
+    global userstate
+    global file_permission
+    file_permission=0
+    print("用户 "+username+" 成功登陆！")
+    userstate[username]=skt
+
+    #send_usertemp(skt)
+
+    #服务器消息处理与转发
+    while(1):
+        msg=skt.recv(100).decode("utf-8")
+        msg=msg.split("$")
+        if(msg[0]=="chat_with_sb"):
+            username1=msg[1]
+            skt1=userstate[username1]
+            newmsg=username+":"+msg[2]
+            sendmsg(skt1,newmsg)
+        if(msg[0]=="online_or_not"):
+            username1=msg[1]
+            if username1 in userstate.keys():
+                sendmsg(skt,"online$")
+            if username1 not in userstate.keys():
+                sendmsg(skt,"offline$")
+        if(msg[0]=="get_usertemp"):
+            send_usertemp(skt)
+        if (msg[0] == "say_byebye"):
+            sendmsg(skt,"byebye$")
+            sendmsg(skt1, "byebye$")
+        if (msg[0] == "receive_file_request"):
+            print("收到文件接收请求:"+str(msg))
+            username1 = msg[1]
+            skt1 = userstate[username1]
+            sendmsg(skt,"receive_file_permission$")
+            file_permission=1
+            msg=skt.recv(100).decode("utf-8")
+            while(file_permission!=2):
+                pass
+            sendmsg(skt1,msg)
+
+        if(msg[0]=="send_file_request"):
+            print("收到文件发送请求:" + str(msg))
+            while(file_permission!=1):
+                pass
+            sendmsg(skt,"send_file_permission$")
+            file_permission=2
+
+        if (msg[0] == "receive_file_over"):
+            file_permission = 0
+
+        if (msg[0] == "logout"):
+            print("已退出:"+msg[1])
+            del userstate[username]
+
+
+
+
+    #chat_with_sb(username, skt, username1)
+
+
+    #a_to_b=threading.Thread(target=chat_with_sb,args=(skt,username))
+    #b_to_a = threading.Thread(target=chat_with_sb, args=(username, username))
+
+def send_usertemp(skt):
+    global userstate
+    # 发送在线名单
+    usertemp = ""
+    try:
+        for user in userstate.keys():
+            usertemp=usertemp+user+"\n"
+    except Exception:
+        usertemp = ""
+    sendmsg(skt, usertemp)
+    return
+
+'''
+def chat_with_sb(username,skt,username1):
+
+    # 我方用户名username,我方socket为skt
+    # 对方用户名username1,对方socket为chatwith
+    chatwith=userstate[username1]
+    while (1):
+        try:
+            data = skt.recv(100)
+            msg = data.decode("utf-8")
+        except Exception:
+            print("异常")
+            break
+        msg_with_head = username + ": " + msg
+        print("收到消息:" + msg_with_head)
+        if(msg=="byebye"):
+            #向对方通知这边退出。msg原始信息（不含用户名头）
+            sendmsg(chatwith,msg)
+            print(username+" 主动退出。")
+            break
+        sendmsg(chatwith,msg_with_head)
+    server_user_online(skt,username)
+'''
+
+
+
+
+
+def tcp_link(conn,addr):
+    #conn.send("Welcome!".encode("utf-8"))
+    while(1):
+        try:
+            data=conn.recv(100)
+            msg=data.decode("utf-8")
+        except Exception:
+            print("一客户端登出")
+            break
+        print("收到消息:"+msg)
+        if(msg.startswith("signup_guide$")):
+            server_signup(conn)
+        if(msg.startswith("login_guide$")):
+            server_login(conn)
+
+
+        if not data:
+            break
+        msg=time.strftime("%y-%m-%d %x")
+        conn.send(msg.encode("utf-8"))
+    conn.close()
+
+
+
+def main():
+    global usermap
+    usermap=shelve.open("userinfo")
+    s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    s.bind(addr0)
+    s.listen(10)
+    print("开始监听")
+
+
+    while(1):
+        conn,addr=s.accept()
+        print("已连接:"+str(addr))
+        t=threading.Thread(target=tcp_link,args=(conn,addr))
+        t.start()
+    s.close()
+    usermap.close()
+
+
+if __name__=="__main__":
+    main()
